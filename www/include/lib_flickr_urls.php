e16ac29563c1cd993c6fe19ff96ce641d20eb16b
kellan <kellan@pobox.com>
2011-11-19 21:20:37 -0500

    display photos either local or from flickr

--- www/include/lib_flickr_urls.php
+++ www/include/lib_flickr_urls.php
@@ -13,15 +13,36 @@
 
 	function flickr_urls_photo_static(&$photo){
 
+		if (!$GLOBALS['cfg']['enable_storagemode']) {
+			return _flickr_urls_photo_local_path($photo);
+
+		} elseif ($photo['storagemode'] == 0) {
+			return _flickr_urls_photo_local_path($photo);
+
+		} elseif ($photo['storagemode'] == 1) {
+			return _flickr_urls_photo_remote_uri($photo);
+		}
+
+	}
+	
+	function _flickr_urls_photo_local_path($photo) {
 		$secret = $photo['secret'];
 		$sz = "z";
 		$ext = "jpg";
-
+		
 		$root = $GLOBALS['cfg']['flickr_static_url'];
 		$path = flickr_photos_id_to_path($photo['id']);
 		$fname = "{$photo['id']}_{$secret}_{$sz}.{$ext}";
 
 		return $root . $path . "/" . $fname;
+	}
+	
+	function _flickr_urls_photo_remote_uri($photo) {
+		$sz = "z";
+		$ext = "jpg";
+
+	    return "http://farm{$photo['farm']}.static.flickr.com/{$photo['server']}/{$photo['id']}_{$photo['secret']}_{$sz}.{$jpg}";
+        
 	}
 
 	#################################################################
